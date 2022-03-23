088162149824da1a227541ab39be9bd4eb8f7986
davidcoats <david@getsafetycone.com>
2022-03-23 14:47:35 -0700

    Initial commit of source-stub.

---
+++ source/R5T.T0128.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0128.Construction
+{
+    /// <summary>
+    /// Construction project for FileContext and FileSet<T> types..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.T0128/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0128
+{
+    /// <summary>
+    /// FileContext and FileSet<T> types.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
